6749d2de8ff69b9bc26cdff01fc45721cbd7544d
michal <michal.kiecana@ncbj.gov.pl>
2022-06-13 11:16:11 +0200

    Stability method temporarily changed

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class SipmType(Enum):
 epsilon = 0.000001
 ip = '10.2.0.126'
 port = 5555
-bar_id = 10
+bar_id = 12
 
 
 def print_hi(name):
@@ -58,11 +58,11 @@ def measure_AFE_voltage_slave(connection: lanconnection.LanConnection):
 
 
 def measure_AFE_all(connection: lanconnection.LanConnection) -> (int, int, int, int, int, int):
-    measured_master_voltage = connection.do_cmd(['adc', bar_id, 3])[1]
-    measured_slave_voltage = connection.do_cmd(['adc', bar_id, 4])[1]
-    measured_master_current = connection.do_cmd(['adc', bar_id, 5])[1]
+    # measured_master_voltage = connection.do_cmd(['adc', bar_id, 3])[1]
+    # measured_slave_voltage = connection.do_cmd(['adc', bar_id, 4])[1]
+    # measured_master_current = connection.do_cmd(['adc', bar_id, 5])[1]
     measured_slave_current = connection.do_cmd(['adc', bar_id, 6])[1]
-    return measured_master_voltage, measured_slave_voltage, measured_master_current, measured_slave_current
+    return 'NULL', 'NULL', 'NULL', measured_slave_current
 
 def measure_AFE_temp(connection: lanconnection.LanConnection) -> (int, int):
     return connection.do_cmd(['gettemp', bar_id])[1]
@@ -91,32 +91,37 @@ def stability(end_date: float, step: float, step_keithley: float, set_voltage: f
         with open(file_name, mode='w', newline='') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=_headers)
             writer.writeheader()
-            time.sleep(waiting_time)
             current_time = time.time()
             print(current_time)
             voltage_result = connection.do_cmd(['setdac', bar_id, set_voltage, set_voltage])
+            time.sleep(waiting_time)
             print(voltage_result)
-            keithley = keithley_util.Keithley6517()
+            # keithley = keithley_util.Keithley6517()
             while current_time < end_date:
                 measure_dict = {}
                 time.sleep(step)
                 result = measure_AFE_all(connection)
-                result_keithley = keithley.measure()
-                r_num = result_keithley['rNum']
-                while result_keithley['rNum'] == r_num:
-                    r_num = result_keithley['rNum']
-                    time.sleep(step_keithley)
-                    result_keithley = keithley.measure()
+                # result_keithley = keithley.measure()
+                # r_num = result_keithley['rNum']
+                # while result_keithley['rNum'] == r_num:
+                #     r_num = result_keithley['rNum']
+                #     time.sleep(step_keithley)
+                    # result_keithley = keithley.measure()
 
                 current_time = time.time()
                 measure_dict[_headers[0]] = current_time
-                measure_dict[_headers[1]] = result_keithley['date'] + ' ' + result_keithley['time']
+                # measure_dict[_headers[1]] = result_keithley['date'] + ' ' + result_keithley['time']
+                measure_dict[_headers[1]] = 'NULL'
                 measure_dict[_headers[2]] = set_voltage
                 measure_dict[_headers[3]] = voltage_result[1][0]
-                measure_dict[_headers[4]] = result_keithley['voltage']
-                measure_dict[_headers[5]] = result[0]
-                measure_dict[_headers[6]] = result[1]
-                measure_dict[_headers[7]] = result[2]
+                # measure_dict[_headers[4]] = result_keithley['voltage']
+                measure_dict[_headers[4]] = 'NULL'
+                # measure_dict[_headers[5]] = result[0]
+                measure_dict[_headers[5]] = 'NULL'
+                # measure_dict[_headers[6]] = result[1]
+                measure_dict[_headers[6]] = 'NULL'
+                # measure_dict[_headers[7]] = result[2]
+                measure_dict[_headers[7]] = 'NULL'
                 measure_dict[_headers[8]] = result[3]
                 writer.writerow(measure_dict)
                 print(current_time)
@@ -194,6 +199,74 @@ def stability_temp(end_date: float, step: float, step_keithley: float, set_volta
             print("Error when hvoff")
         connection.close_connection()
         print(exc)
+
+
+def stability_temp2(end_date: float, step: float, step_keithley: float, set_voltage: float, waiting_time: float,
+              file_name: str):
+    connection = lanconnection.LanConnection(ip, port)
+    try:
+        result = connection.do_cmd(['init', bar_id])
+        if result[0] == 'ERR':
+            print("Error when init")
+        else:
+            print("init OK")
+
+        result = connection.do_cmd(['hvoff', bar_id])
+        if result[0] == 'ERR':
+            print("Error when hvoff")
+        else:
+            print("hvoff OK")
+
+        if os.path.exists(file_name):
+            raise Exception("File already exists!")
+
+        _headers = ('date UTC since epoch [s]', 'date from Keithley', 'Voltage AFE U[V]', 'Temperature SiPM master [bit]', 'Temperature SiPM slave [bit]', 'Temperature keithley [centigrade]')
+
+        with open(file_name, mode='w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=_headers)
+            writer.writeheader()
+            time.sleep(waiting_time)
+            current_time = time.time()
+            print(current_time)
+            voltage_result = connection.do_cmd(['setdac', bar_id, set_voltage, set_voltage])
+            print(voltage_result)
+            keithley = keithley_util.Keithley6517()
+            while current_time < end_date:
+                measure_dict = {}
+                time.sleep(step)
+                result = measure_AFE_temp(connection)
+                result_keithley = keithley.measure()
+                r_num = result_keithley['rNum']
+                while result_keithley['rNum'] == r_num:
+                    r_num = result_keithley['rNum']
+                    time.sleep(step_keithley)
+                    result_keithley = keithley.measure()
+
+                current_time = time.time()
+                measure_dict[_headers[0]] = current_time
+                measure_dict[_headers[1]] = result_keithley['date'] + ' ' + result_keithley['time']
+                measure_dict[_headers[2]] = set_voltage
+                measure_dict[_headers[3]] = result[0]
+                measure_dict[_headers[4]] = result[1]
+                measure_dict[_headers[5]] = result_keithley['temp']
+
+                writer.writerow(measure_dict)
+                print(current_time)
+
+        result = connection.do_cmd(['hvoff', bar_id])
+        if result[0] == 'ERR':
+            print("Error when hvoff")
+        connection.close_connection()
+
+    except Exception as exc:
+        result = connection.do_cmd(['hvoff', bar_id])
+        if result[0] == 'ERR':
+            print("Error when hvoff")
+        connection.close_connection()
+        print(exc)
+
+# def current_calibration_SI(a_int: float, b_int: float, a_ext: float, b_ext: float, start_voltage: float,
+                           # stop_voltage: float, step: float, avg_time: float, step_time: float):
 
 
 def calibration(waiting_time: float, start: int, stop: int, step_voltage: int, step_time: float,
@@ -288,4 +361,7 @@ if __name__ == '__main__':
     # calibration(60, 0, 4095, 64, 12, 0.01, 'calibration_keithley27052022c_master_without_resistor.csv', SipmType.MASTER)
     # calibration(60, 0, 4095, 64, 12, 0.01, 'calibration_keithley27052022c_master_with_resistor.csv', SipmType.MASTER)
     # calibration(600, 0, 4095, 64, 12, 0.01, 'calibration_keithley27052022d_slave_with_resistor.csv', SipmType.SLAVE)
-    stability_temp(1653890400, 20, 0.1, 60.0, 0, 'stability_temp27052022.csv')
+    # stability_temp(1653890400, 20, 0.1, 60.0, 0, 'stability_temp27052022.csv')
+    # calibration(1800, 0, 4095, 64, 12, 0.01, 'calibration_keithley31052022_slave_without_resistor.csv', SipmType.SLAVE)
+    # stability_temp2(1654581600, 20, 0.1, 48.0, 0, 'stability_temp2_06062022.csv')
+    stability(1654758000, 0, 0.1, 59.5, 1800, 'stability_current08062022b.csv')
